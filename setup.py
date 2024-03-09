d193f7696f6074c3d43cd1228b4e8fec030a205b
Jose <jballester1991@gmail.com>
2024-03-09 15:30:21 +0100

    test

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+	name = "ArbRiderAWG",
+	version = "0.1",
+	author = "jobaro",
+	author_email = "dd",
+	description = "Control ARB rider AWG easily from Python",
+	url = "https://github.com/oeito/pyvipc",
+	packages = setuptools.find_packages(),
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent",
+	],
+)
