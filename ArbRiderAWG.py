ae14c2507d14a19d32575578e7c8e3cb38e14e47
Jose <jballester1991@gmail.com>
2024-03-09 16:42:21 +0100

    cositas

--- ArbRiderAWG.py
+++ ArbRiderAWG.py
@@ -15,7 +15,50 @@ class ArbRider:
                         print('Pyvisa not installed')
                         raise
                 self.write("*CLS")
-        
+
+
+        ## Properties #############################################
+                
+        @property
+        def amplitude(self):
+                return self._amplitude
+        @property
+        def function(self):
+                return self._function
+        @property
+        def frequency(self):
+                return self._frequency
+        @property
+        def offset(self):
+                return self._offset
+        @property
+        def output(self):
+                return self._output
+        @property
+        def phase(self):
+                return self._phase   
+        ## Setters #############################################
+
+        @amplitude.setter
+        def amplitude(self,value:float):
+                self._amplitude
+        @function.setter
+        def function(self,value:str):
+                self._function
+        @frequency.setter
+        def frequency(self,value:int):
+                self._frequency
+        @offset.setter
+        def offset(self,value:float):
+                self._offset   
+        @output.setter
+        def output(self,value:int):
+                self._output 
+        @phase.setter
+        def phase(self,value:int):
+                self._phase
+        ## Functions #############################################
+
         def idn(self):
                 return self.resource.query('*IDN?')
         
