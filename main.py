e1989aa56e8174cd8dc0689bbc844e77c05b5f44
Jose <jballester1991@gmail.com>
2024-03-09 14:54:04 +0100

    inicio

---
+++ main.py
@@ -0,0 +1,32 @@
+
+import TeledyneLeCroyPy
+import matplotlib.pyplot as plt
+import numpy as np
+
+o = TeledyneLeCroyPy.LeCroyWaveRunner('VICP::147.156.52.47')
+
+TDIV=4e-3
+VDIV=50e-3
+
+o.set_tdiv(1,TDIV)
+o.set_tdiv(2,TDIV)
+o.set_vdiv(1,VDIV)
+o.set_vdiv(2,VDIV)
+
+o.set_trig_source('C1')
+o.set_trig_slope('Positive')
+o.set_trig_mode('SINGLE')
+data=  o.get_waveform(n_channel=2)
+for waveform in data['waveforms']:
+    time_values = waveform['Time (s)']
+    amplitude_values = waveform['Amplitude (V)']
+
+
+plt.plot(time_values,amplitude_values)
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude (V)')
+plt.title('Datos del Canal 2')
+plt.grid(True)
+plt.show()
+
+
