ef12d87fe2abeecfef5113fb97fd048400690e4f
Jose <jballester1991@gmail.com>
2024-03-09 15:44:34 +0100

    cosas

--- __init__.py
+++ __init__.py
@@ -9,10 +9,12 @@ import numpy as np
 class ArbRiderAWG:
         def __init__(self,resource_name:str):
                 try:
-                        awg=pyvisa.ResourceManager('@py').open_resource(resource_name)
+                        rm=pyvisa.ResourceManager('@py')
+                        self.resource=rm.open_resource(resource_name)
                 except:
                         print('Pyvisa not installed')
-                self.resource=awg
+                        raise
+                self.write("*CLS")
         
         def idn(self):
                 return self.resource.query('*IDN?')
@@ -23,3 +25,11 @@ class ArbRiderAWG:
         def query(self, msg):
                 return self.resource.query(msg)
         
+        def close(self):
+                return self.resource.close()
+        
+        def pulse(self,on,off):
+                return 0
+        
+        
+
