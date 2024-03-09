38e582e3569c9c9899692dd0665abf7dfa340b53
Jose <jballester1991@gmail.com>
2024-03-09 15:52:07 +0100

    test

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import ArbRiderAWG
 import matplotlib.pyplot as plt
 import numpy as np
 
-o = TeledyneLeCroyPy.LeCroyWaveRunner('VICP::') 
+osc = TeledyneLeCroyPy.LeCroyWaveRunner('VICP::') 
 awg = ArbRiderAWG.ArbRider('TCPIP::::INSTR')
 TDIV=4e-3
 VDIV=50e-3 
 
-o.set_tdiv(1,TDIV)
-o.set_tdiv(2,TDIV)
-o.set_vdiv(1,VDIV)
-o.set_vdiv(2,VDIV)
+osc.set_tdiv(1,TDIV)
+osc.set_tdiv(2,TDIV)
+osc.set_vdiv(1,VDIV)
+osc.set_vdiv(2,VDIV)
 
-o.set_trig_source('C1')
-o.set_trig_slope('Positive')
-o.set_trig_mode('SINGLE')
-data=  o.get_waveform(n_channel=2)
+osc.set_trig_source('C1')
+osc.set_trig_slope('Positive')
+osc.set_trig_mode('SINGLE')
+data=  osc.get_waveform(n_channel=2)
 for waveform in data['waveforms']:
     time_values = waveform['Time (s)']
     amplitude_values = waveform['Amplitude (V)']
