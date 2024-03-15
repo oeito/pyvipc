cdad170aa2dbee0e4d2653152103566e7638fc78
oeito <jballester1991@gmail.com>
2024-03-15 11:12:31 +0100

    cosas

--- ArbRiderAWG.py
+++ ArbRiderAWG.py
@@ -1,5 +1,6 @@
 import pyvisa
 import numpy as np
+import time
 
 
 LIMITS = {
@@ -21,12 +22,13 @@ class ArbRider:
                         raise
                 self.ch1= self.Channel(self.rm,1)
                 self.ch2= self.Channel(self.rm,2)
-
+     
         def __del__(self):
                 self.rm.close()    
                  
         def write(self, msg):
                 self.rm.write(msg)
+                time.sleep(0.1)
 
         def writeBinary(self,command:str,values:str):
                 self.rm.write_binary_values(command, values, datatype='d', is_big_endian=False)
@@ -36,6 +38,7 @@ class ArbRider:
 
         def query(self, msg):
                 return self.rm.query(msg)
+                time.sleep(0.1)
         
         def idn(self):
                 return self.query('*IDN?')
@@ -48,19 +51,7 @@ class ArbRider:
                 self.writeBinary(f'WLISt:WAVeform:DATA "{name}",0,{waveform.length}', waveform )
         
 
-        ## Properties #############################################
-        @property
-        def runMode(self):
-                """Set run mode       
-                Returns
-                ------------
-                mode : str
-                        CONTinuous sets Run Mode to Continuous.
-                        TRIGgered sets Run Mode to Triggered.
-                        GATed sets Run Mode to Gated.
-                        SEQuence sets Run Mode to Sequence
-                """
-                return self.query(f'AWGControl:RMODe?')    
+
         @property
         def runState(self):
                 """Query run status      
@@ -72,140 +63,113 @@ class ArbRider:
                         2 indicates that the instrument is running.
                 """
                 return self.query(f'RSTate?')    
-        @property
+
         def run(self):
-                self.write(f'AWGControl:RUN')    
-        @property
+                self.write(f'AFGControl:START')    
+  
         def stop(self):
-                self.write(f'AWGControl:STOP')    
-        @property
+                self.write(f'AFGControl:STOP')    
+
         def trigger(self):
-                self.write(f'*TRG')    
+                self.write(f'TRIGger:SEQuence:IMMediate ')    
 
         ## Setters ################################################
-        @runMode.setter
-        def runMode(self,mode:str):
-                """Set run mode       
-                Parameterers
-                ------------
-                mode : str
-                        CONTinuous sets Run Mode to Continuous.
-                        TRIGgered sets Run Mode to Triggered.
-                        GATed sets Run Mode to Gated.
-                        SEQuence sets Run Mode to Sequence
-                """
-                if mode!=self._runMode:
-                        self._runMode=mode
-                        self.write(f'AWGControl:RMODE {mode}')         
+ 
 
         class Channel:
                 def __init__(self,awg,channel:int):
                         self._awg=awg
                         self._channel=channel
+                        self._output=0
+                        self._amplitude=0
+                        self._pulseWidth=0
+                        self._pulseDutyCycle=0
+                        self._runMode=0
+                        self._trigger=0
+                        self._burst=0
                 ## Properties #############################################
                         
-                @property
-                def amplitude(self):
-                        return self._awg.query(f'SOURce{self._channel}:VOLTage:AMPLitude?')
-                @property
-                def shape(self):
-                        return self._awg.query(f'SOURce{self._channel}:WAVeform?')
-                @property
-                def frequency(self):
-                        return self._awg.query(f'SOURce{self._channel}:FREQuency?')
-                @property
-                def offset(self):
-                        return self._awg.query(f'SOURce{self._channel}:VOLTage:OFFSet?')
-                @property
-                def phase(self):
-                        return self._awg.query(f'SOURce{self._channel}:PHASe:ADJust?') 
-                @property
-                def output(self):
-                        return self._awg.query(f'OUTPut{self._channel}:STATe?')
-                @property
-                def sync(self):
-                        return self._awg.query(f'SOURce{self._channel}:FREQuency:CONCurrent?')  
-
-
-                @property
-                def pulseDutyCycle(self):
-                        return self._awg.query(f'SOURce{self._channel}:PULSe:DCYCle?')      
-                @property
-                def pulsePeriod(self):
-                        return self._awg.query(f'SOURce{self._channel}:PULSe:PERiod?')      
-
-                @property
-                def pulseTransition(self):
-                        return self._awg.query(f'SOURce{self._channel}:PULSe:TRANsition?')      
-
-                @property
-                def pulseWidth(self):
-                        return self._awg.query(f'SOURce{self._channel}:PULSe:WIDTh?')      
 
 
                 ## Setters ################################################
 
-                @amplitude.setter
+
                 def amplitude(self,value:float):
                         if value != self._amplitude:
                                 self._amplitude=value
                                 self._awg.write(f'SOURce{self._channel}:VOLTage:LEVel {value}')
-                @shape.setter
-                def function(self,value:str):
+
+                def shape(self,value:str):
                         if value != self._shape:
                                 self._shape=value
                                 self._awg.write(f'SOURce{self._channel}:WAVeform {value}')
-                @frequency.setter
+   
                 def frequency(self,value:int):
                         if value != self._frequency:
                                 self._frequency=value
                                 self._awg.write(f'SOURce{self._channel}:FREQuency:FIXed {value}')
-                @offset.setter
+ 
                 def offset(self,value:float):
                         if value != self._offset:
                                 self._offset=value
                                 self._awg.write(f'SOURce{self._channel}:VOLTage:LEVel:OFFSet {value}')
-                @phase.setter
+
                 def phase(self,value:int):
                         if value!=self._phase:
                                 self._phase=value
                                 self._awg.write(f'OUTPut{self._channel}:PHASe:ADJust {value}')         
-                @output.setter
+         
                 def output(self,value:int):
                         if value!=self._output:
                                 self._output=value
                                 self._awg.write(f'OUTPut{self._channel}:STATe {value}')         
-                @sync.setter
+          
                 def sync(self,value:int):
                         if value!=self._sync:
                                 self._sync=value
                                 self._awg.write(f'SOURCE{self._channel}:FREQuency:CONCurrent {value}')   
 
+                def burst(self,value:int):
+                        if value!=self._burst:
+                                self._burst=value
+                                self._awg.write(f'SOURCE{self._channel}:BURSt:STATe {value}')   
 
 
 
-                @pulseDutyCycle.setter
+               
                 def pulseDutyCycle(self,value:int):
                         if value!=self._pulseDutyCycle:
                                 self._pulseDutyCycle=value
                                 self._awg.write(f'SOURCE{self._channel}:PULSe:DCYCle {value}')      
 
-                @pulsePeriod.setter
+        
                 def pulsePeriod(self,value:int):
                         if value!=self._pulsePeriod:
                                 self._pulsePeriod=value
                                 self._awg.write(f'SOURCE{self._channel}:PULSe:PERiod {value}')      
 
-                @pulseTransition.setter
+       
                 def pulseTransition(self,value:int):
                         if value!=self._pulseTransition:
                                 self._pulseTransition=value
-                                self._awg.write(f'SOURCE{self._channel}]:PULSe:TRANsition {value}')      
+                                self._awg.write(f'SOURCE{self._channel}:PULSe:TRANsition {value}')      
 
-                @pulseWidth.setter
+          
                 def pulseWidth(self,value:int):
                         if value!=self._pulseWidth:
                                 self._pulseWidth=value
                                 self._awg.write(f'SOURCE{self._channel}:PULSe:WIDTh {value}')      
-
+                def runMode(self,mode:str):
+                        """Set run mode       
+                        Parameterers
+                        ------------
+                        mode : str
+                                CONTinuous sets Run Mode to Continuous.
+                                TRIGgered sets Run Mode to Triggered.
+                                GATed sets Run Mode to Gated.
+                                SEQuence sets Run Mode to Sequence
+                        """
+                        if mode!=self._runMode:
+                                self._runMode=mode
+                                self._awg.write(f'SOURCE{self._channel}:RMODE {mode}')        
                 ## Functions #############################################
--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-
-
 import TeledyneLeCroyPy
 import ArbRiderAWG
 import matplotlib.pyplot as plt
 import numpy as np
-
+import time
 osc = TeledyneLeCroyPy.LeCroyWaveRunner('VICP::') 
-awg = ArbRiderAWG.ArbRider('TCPIP::::INSTR')
+awg = ArbRiderAWG.ArbRider('TCPIP:: ::INSTR')
 TDIV=4e-3
 VDIV=50e-3 
 
-osc.set_tdiv(1,TDIV)
-osc.set_tdiv(2,TDIV)
-osc.set_vdiv(1,VDIV)
-osc.set_vdiv(2,VDIV)
+osc.set_tdiv('10us')
+osc.set_vdiv(3, 50e-3)
 
-osc.set_trig_source('C1')
-osc.set_trig_slope('Positive')
 osc.set_trig_mode('SINGLE')
-data=  osc.get_waveform(n_channel=2)
-awg.ch1.amplitude(1)
+osc.set_trig_source('C3')
+osc.set_trig_slope('C3','Positive')
+
+awg.ch1.burst('ON')
+awg.ch1.output(1)
+awg.ch1.pulseWidth('5us')
+awg.ch1.amplitude(200e-3)
+awg.ch1.pulseDutyCycle(20)
+awg.run()
+awg.trigger()
+data=  osc.get_waveform(n_channel=3)
 for waveform in data['waveforms']:
     time_values = waveform['Time (s)']
     amplitude_values = waveform['Amplitude (V)']
+time.sleep(2)
+   
+awg.ch1.output(0)
 
+awg.stop()
 plt.plot(time_values,amplitude_values)
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude (V)')
 plt.title('Datos del Canal 2')
 plt.grid(True)
 plt.show()
-
+awg.close()
